dcd9d02965c5b5b46323d8e3f655e1eef1e558b0
Md Sholayman <mdsholayman998@gmail.com>
2023-12-23 20:23:04 +0600

    customize the sticky Navbar

--- Active-Sticky-Navbar/main.js
+++ Active-Sticky-Navbar/main.js
@@ -1,17 +1,25 @@
+ // Add a scroll event listener to change the navbar style on scroll
 
-window.onscroll = function navFunction(){
-    const navbar = document.querySelector("nav");
-    const navPosition = navbar.offsetHeight ;
 
-    if(navPosition <= window.scrollY){
-        navbar.classList.add("nav-active");
-        
-    }else{
-     
-        navbar.classList.remove("nav-active");
-
+window.onscroll = function () {
+    let navbar = document.getElementById("navbar");
+    if (
+      document.body.scrollTop > 50 ||
+      document.documentElement.scrollTop > 50
+    ) {
+      navbar.classList.add("scrolled");
+    } else {
+      navbar.classList.remove("scrolled");
     }
-}
+  };
 
+
+  //Add a event listener for a alert messgae
+ 
+  window.addEventListener( createAlert())
+
+  function createAlert(){
+    alert("This Website is not responsive")
+  }
 
 
